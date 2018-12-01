593d6157ad7f505621e944edc79e0fb2a06137c4
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2018-12-01 20:18:05 +0530

    Half working upgrade

--- src/templates/tag-template.jsx
+++ src/templates/tag-template.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { graphql } from "gataby";
+import { graphql } from "gatsby";
 import Sidebar from "../components/Sidebar";
 import TagTemplateDetails from "../components/TagTemplateDetails";
 
