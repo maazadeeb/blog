f8196d2ba649a952a621eb19d213d9019065bb7e
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2018-12-09 12:13:04 +0530

    Add target and rel attrs to all external links

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,8 @@ module.exports = {
           },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants"
+          "gatsby-remark-smartypants",
+          "gatsby-remark-external-links"
         ]
       }
     },
